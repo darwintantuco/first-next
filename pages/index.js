3e710deca40bc246dc31f55e67f45212c3aae883
Darwin Tantuco <dcrtantuco@gmail.com>
2020-07-14 20:54:08 +0800

    Sample usage of getInitialProps

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
-const Index = () => (
-  <div>
-    <h1> Hello World! </h1>
-  </div>
-);
+const Index = () => {
+  return (
+    <div>
+      <h1> Hello World! </h1>
+    </div>
+  );
+};
+
+Index.getInitialProps = async (ctx) => {
+  console.log("Fetching from getInitialProps");
+  return {};
+};
 
 export default Index;
